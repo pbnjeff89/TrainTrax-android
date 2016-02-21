001c54797c8526ffd6d1650981843154816c596a
pbnjeff89 <jeffdamasco@gmail.com>
2016-02-21 15:20:46 -0600

    New exercise class and exercise layout:
    - Created a new exercise class which can take in the exercise name,
      the number of reps performed, weight used, and RPE
    - Modified TrackActivity.java to populate a ListView on the layout
    - Created a new layout file to generalize the structure for each item on
      the list of exercises

---
+++ app/src/main/java/com/pbnjeff/wot/Exercise.java
@@ -0,0 +1,36 @@
+package com.pbnjeff.wot;
+
+/**
+ * Created by Jeff on 2/21/2016.
+ */
+public class Exercise {
+    private String name;
+    private double weight;
+    private int reps;
+    private double rpe;
+
+    public Exercise(String name, double weight, int reps, double rpe) {
+        super();
+        this.name = name;
+        this.weight = weight;
+        this.reps = reps;
+        this.rpe = rpe;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public double getRPE() {
+        return rpe;
+    }
+
+}
--- app/src/main/java/com/pbnjeff/wot/TrackActivity.java
+++ app/src/main/java/com/pbnjeff/wot/TrackActivity.java
@@ -2,61 +2,96 @@ package com.pbnjeff.wot;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrackActivity extends AppCompatActivity {
 
-    /*private LinearLayout newLayout;
-    private EditText weightText, repText, rpeText;
-    private Button addButton;*/
+    private List<Exercise> myExercises = new ArrayList<Exercise>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
 
-        /*Spinner spinner = (Spinner) findViewById(R.id.exercise_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.exercises_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        populateExerciseList();
+        populateListView();
 
-        newLayout = (LinearLayout) findViewById(R.id.exercise_sets);
-        weightText = (EditText) findViewById(R.id.weight_input);
-        repText = (EditText) findViewById(R.id.reps_input);
-        rpeText = (EditText) findViewById(R.id.rpe_input);
-        addButton = (Button) findViewById(R.id.add_button);
-        addButton.setOnClickListener(onClick());
-        TextView textView = new TextView(this);
-        textView.setText("lol");*/
+        final ListView listview = (ListView) findViewById(R.id.exercise_list);
+
     }
 
-    /**
+    private void populateExerciseList() {
+        myExercises.add(new Exercise("Squat", 132.5, 10, 1));
+        myExercises.add(new Exercise("Bench", 100, 10, 2));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+        myExercises.add(new Exercise("Deadlift", 150, 10, 3));
+    }
 
-    private View.OnClickListener onClick() {
+    private void populateListView() {
+        ArrayAdapter<Exercise> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.exercise_list);
+        list.setAdapter(adapter);
+    }
 
-        // Please clear the EditText each time the button is pressed
+    private class MyListAdapter extends ArrayAdapter<Exercise> {
 
-        return new View.OnClickListener() {
+        public MyListAdapter() {
+            super(TrackActivity.this, R.layout.exercise_view, myExercises);
+        }
 
-            @Override
-            public void onClick (View view) {
-                newLayout.addView(createNewTextView(weightText.getText().toString() +
-                        " x " + repText.getText().toString() +
-                        " @ " + rpeText.getText().toString()));
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.exercise_view, parent, false);
             }
-        };
+
+            Exercise currentExercise = myExercises.get(position);
+
+            TextView exerciseName = (TextView) itemView.findViewById(R.id.list_exercise_name);
+            exerciseName.setText(currentExercise.getName());
+
+            TextView weightUsed = (TextView) itemView.findViewById(R.id.exercise_weight_used);
+            String weight = String.valueOf(currentExercise.getWeight());
+            weightUsed.setText(weight);
+
+            TextView repsPerformed = (TextView) itemView.findViewById(R.id.list_exercise_reps);
+            String reps = String.valueOf(currentExercise.getReps());
+            repsPerformed.setText(reps);
+
+            TextView rpe = (TextView) itemView.findViewById(R.id.list_exercise_RPE);
+            String rpeString = String.valueOf(currentExercise.getRPE());
+            rpe.setText(rpeString);
+
+            return itemView;
+        }
+
     }
-
-    private TextView createNewTextView(String text) {
-        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        final TextView textView = new TextView(this);
-
-        textView.setLayoutParams(lparams);
-        textView.setText(text);
-        return textView;
-    }
-
-     **/
 
 }
