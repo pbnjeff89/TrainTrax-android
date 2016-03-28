9ec3c55d941e55e3e917f897f9220e9f073aa86d
Jeff Damasco <jeffdamasco@gmail.com>
2016-03-28 16:36:10 -0500

    Implemented exchange between Track and Add Exercise activities
    - User may change EditText in Add Exercise activity which will filter the
      ListView
    - User can select the exercise to add into the list in Track
    - Track activity will take the intended exercise to add and append it to
      the exercise list hash map and list

--- app/src/main/java/com/pbnjeff/wot/AddExerciseActivity.java
+++ app/src/main/java/com/pbnjeff/wot/AddExerciseActivity.java
@@ -1,28 +1,51 @@
 package com.pbnjeff.wot;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddExerciseActivity extends AppCompatActivity {
 
-    AlertDialog.Builder addExerciseAlertBuilder;
     ListView addExerciseList;
     EditText searchExercise;
 
     Button addConfirm;
-    Button addCancel;
+
+    Resources res;
+    String[] exerciseDB;
+
+    ArrayAdapter<String> adapter;
+
+    String exerciseToReturn;
+    TextView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+
+        exerciseToReturn = "";
+        preview = (TextView) findViewById(R.id.add_preview);
+        preview.setText("Exercise to add: " + exerciseToReturn);
+
+        // set up exercise list array
+
+        res = getResources();
+        exerciseDB = res.getStringArray(R.array.exercises);
 
         // configure EditText
 
@@ -35,24 +58,81 @@ public class AddExerciseActivity extends AppCompatActivity {
         addConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = getIntent();
-                intent.putExtra("addExercise", searchExercise.getText().toString());
-                setResult(RESULT_OK, intent);
-                finish();
+                if(exerciseToReturn.equals("")) {
+                    Toast.makeText(AddExerciseActivity.this,
+                            "Please select a an exercise",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent intent = getIntent();
+                    intent.putExtra("addExercise", exerciseToReturn);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
             }
         });
 
-        addCancel = (Button) findViewById(R.id.add_cancel);
+        // configure listview
 
-        addCancel.setOnClickListener(new View.OnClickListener() {
+        addExerciseList = (ListView) findViewById(R.id.lv_add_exercise);
+
+        // configure listview adapter
+
+        adapter = new ArrayAdapter<String>(AddExerciseActivity.this,
+                R.layout.add_exercise_layout_row,
+                R.id.add_exercise_row,
+                exerciseDB);
+        addExerciseList.setAdapter(adapter);
+
+        // set EditText behavior
+
+        searchExercise.addTextChangedListener(new TextWatcher() {
 
             @Override
-            public void onClick(View v) {
-                Intent intent = getIntent();
-                 setResult(RESULT_CANCELED, intent);
-                finish();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+
+        // set up listener for listview
+
+        addExerciseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String exerciseSelected = adapter.getItem(position).toString();
+                confirmAdd(exerciseSelected);
+            }
+
+        });
+
+    }
+
+    private void confirmAdd(final String exerciseSelected) {
+
+        AlertDialog.Builder addAlertBuilder = new AlertDialog.Builder(AddExerciseActivity.this)
+                .setMessage("Do you want to add " + exerciseSelected + "?")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        exerciseToReturn = exerciseSelected;
+                        preview.setText("Exercise to add: " + exerciseToReturn);
+                    }
+                })
+                .setNegativeButton("Cancel", null);
+        AlertDialog addAlert = addAlertBuilder.create();
+        addAlert.show();
 
     }
 }
